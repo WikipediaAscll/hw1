9cbb1545b3005a315c0d85be5c613453a891519f
Amber_lo <WikipediaAscll@gmail.com>
2020-10-17 23:28:55 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+WebFont.load({
+	google: {
+	families: ['Noto Sans TC']
+	}
+});
+
+	$( document ).ready(function() {
+//     $( ".show_btn" ).click(function() {
+// 		$(".lightbox").toggle(); 
+// 	});
+});	
